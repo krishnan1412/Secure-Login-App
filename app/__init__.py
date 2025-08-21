3695943bbe9f55edfc145292a5447e19e67626c6
krishnan1412 <167602903+krishnan1412@users.noreply.github.com>
2025-08-21 12:34:30 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
+import boto3
+import json
 from flask import Flask
 from app.routes import login_bp
 
+def get_secret():
+    client = boto3.client('secretsmanager', region_name='ap-northeast-2')
+    response = client.get_secret_value(SecretId='login-app-secrets')
+    secret_dict = json.loads(response['SecretString'])
+    return secret_dict['SECRET_KEY']
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'  # Replace with AWS Secrets Manager
+    app.config['SECRET_KEY'] = get_secret()
     app.register_blueprint(login_bp)
     return app
